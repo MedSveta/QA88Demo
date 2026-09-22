a803ff269474f10063512ecde13a474771c7979c
MedSveta <sveta1978medved@gmail.com>
2026-09-22 15:25:33 +0300

    Start

--- tests/test_selectors_css.py
+++ tests/test_selectors_css.py
@@ -1,6 +1,19 @@
 import time
+from selenium.webdriver.common.by import By
 
 
 class TestSelectorsCss:
     def test_selectors_css(self, driver):
-        """"""
+        time.sleep(2)
+        footer = driver.find_element(By.TAG_NAME, "footer")
+        print(footer.tag_name)
+
+        tools = driver.find_element(By.CSS_SELECTOR, "img[src='/assets/Toolsqa-DZdwt2ul.jpg']")
+        print(tools.get_attribute('src'))
+
+        driver.find_element(By.CSS_SELECTOR, "a[href='/elements']").click()
+        time.sleep(2)
+
+        driver.back()
+
+
